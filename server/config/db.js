20bec64e26e8f1da608c1a8a75d8b7897e7f0ad2
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-08 10:21:56 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/creditloan', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect('mongodb://localhost:27017/creditloan');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
