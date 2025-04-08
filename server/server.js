a7d49cf96c581874aca6392ce089eb177f7a7480
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-08 21:17:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  origin :"*",
+));
 app.use(express.json());
 
 connectDB();
