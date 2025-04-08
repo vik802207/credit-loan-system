d7e3ad5194feedd9c795fb4fb19afdb311ad7ae3
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-08 17:10:20 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
+dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log('✅ MongoDB connected');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1); // Exit process on DB failure
   }
 };
 
