0738ffb1702707820b25e88f41ffea2493866c08
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-08 10:43:42 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('https://cloud.mongodb.com/v2/67f4ad63aba9ed04f4542f2f#/metrics/replicaSet/67f4add002cb0b7b0dde4684/explorer/sample_mflix');
+    await mongoose.connect('mongodb+srv://vikashg802207:mpv5Kosp5LeY7wf1@credit-loan.sksmh36.mongodb.net/?retryWrites=true&w=majority&appName=credit-loan');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
