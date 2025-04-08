d84cc0dd486dfbdaa4cddaf774fedcd55b6fcb7b
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-04-08 10:34:05 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/creditloan');
+    await mongoose.connect('https://cloud.mongodb.com/v2/67f4ad63aba9ed04f4542f2f#/metrics/replicaSet/67f4add002cb0b7b0dde4684/explorer/sample_mflix');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
